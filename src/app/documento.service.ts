1d4f7b4bc4a30437d2eb165f8d36bcfe3433ebd4
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 12:49:59 -0500

    agregar metodo para listar documentos en servicio

--- src/app/documento.service.ts
+++ src/app/documento.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Documento } from './documento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentoService {
 
-  constructor() { }
+  //Esta URL obtiene el listado de todos los documentos en el backend
+  private baseURL = "https://yasminmontoya-registro-documentos.up.railway.app/api/documentos";
+
+  constructor(private httpClient : HttpClient) { }
+
+  //este metodo nos sirve para obtener los documentos
+  obtenerListaDeDocumentos():Observable<Documento[]>{
+    return this.httpClient.get<Documento[]>(`${this.baseURL}`);
+  }
 }
