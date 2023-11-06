55599de9eaa30b5117fd9693fa6f6b4542cc1500
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 13:16:22 -0500

    agregar ruta para registrar-documento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.component';
+import { RegistrarDocumentoComponent } from './registrar-documento/registrar-documento.component';
 
 const routes: Routes = [
     {path: 'documentos', component:ListaDocumentosComponent},
     {path: '', redirectTo: 'documentos', pathMatch: 'full'},
+    {path: 'registrar-documento', component:RegistrarDocumentoComponent},
 ];
 
 @NgModule({
