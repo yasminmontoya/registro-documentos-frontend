5ae2966004fc99466d8d2d7d37f1ce7351d7907b
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 12:56:38 -0500

    agregar ruta para listar documentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'documentos', component:ListaDocumentosComponent},
+    {path: '', redirectTo: 'documentos', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
