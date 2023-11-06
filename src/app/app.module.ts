bc52cbc75ab6556657a089de63288dcc3a0fa30e
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 11:29:50 -0500

    importar HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
