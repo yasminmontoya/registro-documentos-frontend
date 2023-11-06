f4e732a607b27381371ec9facc1300cb7ccf9551
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 13:16:49 -0500

    agregar navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RegistrarDocumentoComponent } from './registrar-documento/registrar-documento.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaDocumentosComponent
+    ListaDocumentosComponent,
+    RegistrarDocumentoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
