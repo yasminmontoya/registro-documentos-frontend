19e1c28a0426870d691600630ef62d0053b0a5b1
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 18:14:29 -0500

    crear componente para actualizar un documento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.co
 import { HttpClientModule } from '@angular/common/http';
 import { RegistrarDocumentoComponent } from './registrar-documento/registrar-documento.component';
 import { FormsModule } from '@angular/forms';
+import { ActualizarEmpleadoComponent } from './actualizar-empleado/actualizar-empleado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListaDocumentosComponent,
-    RegistrarDocumentoComponent
+    RegistrarDocumentoComponent,
+    ActualizarEmpleadoComponent
   ],
   imports: [
     BrowserModule,
