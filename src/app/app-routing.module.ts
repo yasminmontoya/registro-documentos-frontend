12a024f83aceaa843e8415bcf389d9f4de340321
yasminmontoya <yaz.lorena@gmail.com>
2023-11-06 20:36:04 -0500

    agregar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaDocumentosComponent } from './lista-documentos/lista-documentos.component';
 import { RegistrarDocumentoComponent } from './registrar-documento/registrar-documento.component';
-import { ActualizarDocumentoComponent } from './actualizar-documento/actualizar-documento.component';
-import { DetalleDocumentoComponent } from './detalle-documento/detalle-documento.component';
 
 const routes: Routes = [
     {path: 'documentos', component:ListaDocumentosComponent},
     {path: '', redirectTo: 'documentos', pathMatch: 'full'},
     {path: 'registrar-documento', component:RegistrarDocumentoComponent},
-    {path: 'actualizar-documento/:id', component:ActualizarDocumentoComponent},
-    {path: 'detalle-documento/:id', component:DetalleDocumentoComponent}
 ];
 
 @NgModule({
