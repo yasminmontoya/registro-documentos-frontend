5ebeba5910d6e6c912acc1b80dae15094696a90b
yasminmontoya <yaz.lorena@gmail.com>
2023-11-10 23:48:07 -0500

    agregar informacion al detalle del documento

--- src/app/components/detalle-documento/detalle-documento.component.ts
+++ src/app/components/detalle-documento/detalle-documento.component.ts
@@ -3,6 +3,8 @@ import { Documento } from '../../models/documento';
 import { DocumentoService } from '../../services/documento.service';
 import { ActivatedRoute } from '@angular/router';
 import swal from 'sweetalert2';
+import { TipoService } from 'src/app/services/tipo.service';
+import { ProcesoService } from 'src/app/services/proceso.service';
 
 @Component({
   selector: 'app-detalle-documento',
@@ -12,8 +14,10 @@ import swal from 'sweetalert2';
 export class DetalleDocumentoComponent {
   id:number;
   documento:any;
+  tipo:any;
+  proceso:any;
 
-  constructor(private documentoServicio:DocumentoService, private route:ActivatedRoute) {
+  constructor(private documentoServicio:DocumentoService, private tipoServicio:TipoService, private procesoServicio:ProcesoService, private route:ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -21,6 +25,12 @@ export class DetalleDocumentoComponent {
     this.documento = new Documento();
     this.documentoServicio.obtenerDocumentoPorId(this.id).subscribe(dato => {
       this.documento = dato;
+      this.tipoServicio.obtenerTipoPorId(this.documento.tipo_id).subscribe(dato => {
+        this.tipo = dato;
+      });
+      this.procesoServicio.obtenerProcesoPorId(this.documento.tipo_id).subscribe(dato => {
+        this.proceso = dato;
+      });
       swal(`Detalles del documento ${this.documento.nombre}`);
     });
   }
