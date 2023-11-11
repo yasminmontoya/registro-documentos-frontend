5acfd4a47439d17701da9513384807424e9c4bf2
yasminmontoya <yaz.lorena@gmail.com>
2023-11-11 00:28:42 -0500

    agregar select a vista actualizar documento

--- src/app/components/actualizar-documento/actualizar-documento.component.ts
+++ src/app/components/actualizar-documento/actualizar-documento.component.ts
@@ -3,6 +3,10 @@ import { DocumentoService } from '../../services/documento.service';
 import { Documento } from '../../models/documento';
 import { ActivatedRoute, Router } from '@angular/router';
 import swal from 'sweetalert2';
+import { TipoService } from 'src/app/services/tipo.service';
+import { Tipo } from 'src/app/models/tipo';
+import { ProcesoService } from 'src/app/services/proceso.service';
+import { Proceso } from 'src/app/models/proceso';
 
 @Component({
   selector: 'app-actualizar-documento',
@@ -12,7 +16,10 @@ import swal from 'sweetalert2';
 export class ActualizarDocumentoComponent {
   id:number;
   documento:any;
-  constructor(private documentoService:DocumentoService,private router:Router,private route:ActivatedRoute) { }
+  tipos: Tipo[];
+  procesos: Proceso[];
+
+  constructor(private documentoService:DocumentoService,private tipoServicio:TipoService, private procesoServicio:ProcesoService, private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -20,6 +27,8 @@ export class ActualizarDocumentoComponent {
     this.documentoService.obtenerDocumentoPorId(this.id).subscribe(dato =>{
       this.documento = dato;
     },error => console.log(error));
+    this.obtenerTipos();
+    this.obtenerProcesos();
   }
 
   irAlaListaDeDocumentos(){
@@ -32,4 +41,17 @@ export class ActualizarDocumentoComponent {
       swal('Documento actualizado',`El documento ${this.documento.nombre} ha sido actualizado con exito`,`success`);
     },error => console.log(error));
   }
+
+  private obtenerTipos(){
+    this.tipoServicio.obtenerListaDeTipos().subscribe(dato => {
+      this.tipos = dato;
+    });
+  }
+
+  private obtenerProcesos(){
+    this.procesoServicio.obtenerListaDeProcesos().subscribe(dato => {
+      this.procesos = dato;
+    });
+  }
+
 }
